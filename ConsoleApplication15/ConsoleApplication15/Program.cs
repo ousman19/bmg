be8527a0b3e0793bc03b0b331fb0e69cbe2ecae7
ousman19 <34682424+ousman19@users.noreply.github.com>
2017-12-20 12:56:12 +0300

    Add files via upload

---
+++ ConsoleApplication15/ConsoleApplication15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            Console.WriteLine("Adınızı Giriniz:");
+            x = Console.ReadLine();
+            foreach (char y in x)
+            Console.Write((char)(y + 10));
+            Console.ReadKey();
+        }
+    }
+}
